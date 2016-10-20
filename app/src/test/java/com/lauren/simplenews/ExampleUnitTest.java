436e269fbf25665b8b42475e1a30b3282e52faa7
tan huohui <carythh@users.noreply.github.com>
2016-10-20 09:53:08 +0800

    ddd

--- app/src/test/java/com/lauren/simplenews/ExampleUnitTest.java
+++ app/src/test/java/com/lauren/simplenews/ExampleUnitTest.java
@@ -8,6 +8,9 @@ import static org.junit.Assert.*;
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
 public class ExampleUnitTest {
+    
+    // 测试
+    
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
